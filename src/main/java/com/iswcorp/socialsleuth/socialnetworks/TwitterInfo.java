15fa946af36e8f9c8fbf8f24b73d0a498aafdc03
berkoab <aryeh.berkowitz@gmail.com>
2015-02-11 14:49:48 -0500

    added twitter4j to pom

--- src/main/java/com/iswcorp/socialsleuth/socialnetworks/TwitterInfo.java
+++ src/main/java/com/iswcorp/socialsleuth/socialnetworks/TwitterInfo.java
@@ -1,5 +1,12 @@
 package com.iswcorp.socialsleuth.socialnetworks;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 public class TwitterInfo {
 	private String username;
 	private String id;
@@ -13,5 +20,23 @@ public class TwitterInfo {
 	}
 	public void setLocation(String location) {
 		this.location = location;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getLocation() {
+		return location;
+	}
+	//example twitter search - http://twitter4j.org/en/code-examples.html
+	public void searchForTweets(String username) throws TwitterException {
+		Twitter twitter = TwitterFactory.getSingleton();
+	    Query query = new Query("source:twitter4j "+username);
+	    QueryResult result = twitter.search(query);
+	    for (Status status : result.getTweets()) {
+	        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+	    }
 	}
 }
